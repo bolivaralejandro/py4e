8611f74583eb720e7ceef36dc8fe0c22a5c0b6a2
Alejandro Bolivar <abolivar@abolivar-ltm1.internal.salesforce.com>
2022-11-01 15:56:48 -0400

    added find_host.py

--- find_host.py
+++ find_host.py
@@ -1,8 +1,12 @@
+# the re way below look for '@' sign then # followed by non blank characters '^ ' the * means
+# many of them or 0 or more times
+#
 import re
 lin = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
 y = re.findall('^From .*@([^ ]*)',lin)
 print(y)
 
+# The split method
 line = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
 words = line.split()
 email = words[1]
